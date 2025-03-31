81e4c785b20ad8bc18c30eaedd2560862c79f3f9
RDayvson <90219699+RDayvson@users.noreply.github.com>
2025-03-31 10:01:12 -0300

    Create charge_model.py
    Carrega o modelo pré-treinado

---
+++ charge_model.py
@@ -0,0 +1,5 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+model_name = "neuralmind/bert-base-portuguese-cased"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)  # 2 classes: ameaça ou não
